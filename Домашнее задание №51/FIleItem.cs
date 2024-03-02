e9727451b284768e5a0841a79ccaf29011330e06
krino45 <krina645@gmail.com>
2024-03-02 22:56:59 +0700

    HW №5 with implementations of the additional tasks. Also put the FileItem class into a different file.

---
+++ Домашнее задание №51/FIleItem.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia.Media.Imaging;
+
+namespace ExplorerAtHome
+{
+    public class FileItem
+    {
+        private string _filePath;
+        private Bitmap _iconPath;
+        private string? _fullfilePath;
+
+        public FileItem(string filePath, string iconPath, string? fullfilePath = "")
+        {
+            _filePath = filePath;
+            _iconPath = new Bitmap(iconPath);
+            _fullfilePath = fullfilePath;
+        }
+
+        public string FilePath { get => _filePath; }
+        public Bitmap IconPath { get => _iconPath; }
+        public string? FullfilePath { get => _fullfilePath; }
+
+    }
+}
